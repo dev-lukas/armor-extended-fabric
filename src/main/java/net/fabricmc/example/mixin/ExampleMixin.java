c0d870defe846a2cd7530425fd32ce6a4b139460
BuildTools <unconfigured@null.spigotmc.org>
2022-11-13 14:37:03 +0100

    First Changes

--- src/main/java/net/fabricmc/example/mixin/ExampleMixin.java
+++ src/main/java/net/fabricmc/example/mixin/ExampleMixin.java
@@ -1,16 +1,20 @@
 package net.fabricmc.example.mixin;
 
 import net.fabricmc.example.ExampleMod;
-import net.minecraft.client.gui.screen.TitleScreen;
+import net.minecraft.entity.DamageUtil;
+import net.minecraft.util.math.MathHelper;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
-@Mixin(TitleScreen.class)
+@Mixin(DamageUtil.class)
 public class ExampleMixin {
-	@Inject(at = @At("HEAD"), method = "init()V")
-	private void init(CallbackInfo info) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
-	}
+	@Inject(method = "getDamageLeft(FFF)V", at = @At("RETURN"), cancellable = true, locals = LocalCapture.CAPTURE_FAILSOFT)
+	public static float getDamageLeft(float damage, float armor, float armorToughness, CallbackInfoReturnable<Float> cir) {
+        float f = 2.0f + armorToughness / 4.0f;
+        float g = MathHelper.clamp(armor - damage / f, armor * 0.2f, 20.0f);
+        return damage * (1.0f - g / 25.0f);
+    }
 }
