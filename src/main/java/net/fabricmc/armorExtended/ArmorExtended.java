498df36546fc1c3b4d6c5b540c43d3c1d84af282
BuildTools <unconfigured@null.spigotmc.org>
2022-11-24 00:12:19 +0100

    Fixing leftover default modid entries

--- src/main/java/net/fabricmc/armorExtended/ArmorExtended.java
+++ src/main/java/net/fabricmc/armorExtended/ArmorExtended.java
@@ -8,7 +8,7 @@ public class ArmorExtended implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+	public static final Logger LOGGER = LoggerFactory.getLogger("armorextended_fabric");
 	public static final ArmorExtendedConfig CONFIG = ArmorExtendedConfig.createAndLoad();
 
 	@Override
